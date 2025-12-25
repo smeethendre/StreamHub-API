1e7175e052645a62a58775ca4cba80ba20300126
Smeet Hendre <smeethendre4@gmail.com>
2025-12-25 16:51:39 +0530

    feat: add jwt for token generation and enhance user model methods

--- src/middleware/auth.middleware.js
+++
@@ -1,5 +0,0 @@
-import User from "../model/user.model.js";
-import { ApiError } from "../util/apiError.js";
-import asyncHandler from "../util/asyncHandler.js";
-
-asyncHandler, ApiError, User
--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,11 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
   userId: {
     type: String,
     required: true,
+  },
+
+  email: {
+    type: String,
+    require: true,
+    trim: true,
   },
 
   password: {
@@ -30,5 +37,22 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10); //if modified or new, hash it.
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      userName: this.userName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXP,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
